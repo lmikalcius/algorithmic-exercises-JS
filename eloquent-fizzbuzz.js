db5e19b1c82b25a2fd832a53f0f845845dda8437
linas1985 <linas.mikalcius@gmail.com>
2016-02-25 16:25:57 -0600

    Add FizzBuzz

---
+++ eloquent-fizzbuzz.js
@@ -0,0 +1,9 @@
+for (var i = 1; i <= 100; i++) {
+  var output = "";
+  if (i % 3 === 0) {
+    output += "Fizz";
+  } else if (i % 5 === 0) {
+    output += "Buzz";
+  }
+  console.log(output || i);
+}
