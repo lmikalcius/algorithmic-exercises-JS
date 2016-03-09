de84eb0229aa4d72437fe3f76e53fd4c3e578f5d
linas1985 <linas.mikalcius@gmail.com>
2016-03-09 16:48:24 -0600

    Add sort to deep comparison operator

--- deep-comparison-operator.js
+++ deep-comparison-operator.js
@@ -1,33 +1,29 @@
-function deepEqual(value1, value2) {
-  if (value1 === value2) {
-    return true;
-  } else if ((typeof value1 === "object" && value1 !== null) &&
-             (typeof value2 === "object" && value2 !== null)) {   // If they are objects and not null do a deep comparison
-    if (!arraysEqual(Object.keys(value1), Object.keys(value2))) { // Check to see if the keys of the objects are equal
-      return false;
-    }
-    for (var prop in value1) { // Check to see if the values of each key are equal
-      if (!deepEqual(value1[prop], value2[prop])) {
-        return false;
-      }
+function deepComparison(value1, value2) {
+  if ((typeof value1 === "object" && value1 !== null) &&
+      (typeof value2 === "object" && value2 !== null)) {
+    for (prop in value1) {
+      if (!compareKeys(value1, value2)) return false;
+      if (!deepComparison(value1[prop], value2[prop])) return false;
     }
     return true;
   } else {
-    return false;
+    return value1 === value2;
   }
 }
 
-function arraysEqual(arr1, arr2) {
-  if(arr1.length !== arr2.length) {
-    return false;
-  }
-  for(var i = arr1.length; i--;) {
-    if(arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
+function compareKeys(obj1, obj2) {
+  keys1 = Object.keys(obj1).sort;
+  keys2 = Object.keys(obj2).sort;
+  if (keys1.length !== keys2.length) return false;
+  for (var i = 0; i < keys1.length; i++)
+    if (keys1[i] !== keys2[i]) return false;
   return true;
 }
+
+var obj1 = {name: "Linas", stuff: "extra", rest: {middle: "Richard", last: "Mikalcius"}};
+var obj2 = {name: "Linas", rest: {middle: "Richard", last: "Mikalcius"}, stuff: "extra"};
+
+console.log(deepComparison(obj1, obj2));
 
 
 // Eloquent JavaScript way
